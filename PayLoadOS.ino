3a59c4e2b5484be81be6301ccb6e830b19f6dba8
Harrison <bonnerharrison2004@gmail.com>
2023-09-28 18:18:48 -0500

    switched to dc motor

--- PayLoadOS.ino
+++ PayLoadOS.ino
@@ -2,78 +2,65 @@
 #include <Adafruit_BMP3XX.h>
 #include <AccelStepper.h>
 
-#define ALTITUDE_THRESHOLD 1000  // Set your desired altitude threshold in meters
+#define ALTITUDE_THRESHOLD 1000 // Set your desired altitude threshold in meters
 #define RELAY_PIN_1 2
 #define RELAY_PIN_2 3
 #define STEPPER_ENABLE_PIN 4
 #define STEPPER_STEP_PIN 5
 #define STEPPER_DIR_PIN 6
 #define STEPS_PER_REV 200       // Set the steps per revolution for your stepper motor
-#define SEALEVELPRESSURE_HPA 10
+#define SEALEVELPRESSURE_HPA 10 // Will need to be calibrated
 
-Adafruit_BMP3XX bmp;
-AccelStepper stepper(AccelStepper::FULL4WIRE, STEPPER_STEP_PIN, STEPPER_DIR_PIN);
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+#define motorRelay 8
+Adafruit_BMP3XX bmp; // I2C
+// Adafruit_BMP3XX bmp(BMP_CS); // hardware SPI
+// dafruit_BMP3XX bmp(BMP_CS, BMP_MOSI, BMP_MISO, BMP_SCK);
 
-bool stepperActivated = false;
-bool stepperDone = false;
 int altitudeCount = 0;
 
-int motorRev = 5;
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  if (!bmp.begin_I2C()) {
+  if (!bmp.begin_I2C())
+  {
     Serial.println("Could not find a valid BMP3 sensor, check wiring!");
-    while (1);
+    while (1)
+      ;
   }
 
   pinMode(RELAY_PIN_1, OUTPUT);
   pinMode(RELAY_PIN_2, OUTPUT);
-  pinMode(STEPPER_ENABLE_PIN, OUTPUT);
-
-  // Set the maximum speed and acceleration for the stepper motor
-  stepper.setMaxSpeed(1000);
-  stepper.setAcceleration(500);
-
-  // Enable the stepper motor driver
-  digitalWrite(STEPPER_ENABLE_PIN, LOW);
+  pinMode(motorRelay, OUTPUT);
 }
 
-void loop() {
-  //System Checking loop until we meet threshold
-  if (!stepperActivated) {
+void loop()
+{
+  // System Checking loop until we meet threshold
 
-    //Get current altitude
-    float currentAltitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  // Get current altitude
+  float currentAltitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
 
-    Serial.print("Altitude: ");
-    Serial.print(currentAltitude);
-    Serial.println(" meters");
-    //Check to see if it is the threshold, we technically need to hit it twice
-    if (currentAltitude == ALTITUDE_THRESHOLD) {
-      altitudeCount++;
+  Serial.print("Altitude: ");
+  Serial.print(currentAltitude);
+  Serial.println(" meters");
+  // Check to see if it is the threshold, we technically need to hit it twice
+  if (currentAltitude == ALTITUDE_THRESHOLD)
+  {
+    altitudeCount++;
 
-      //if it is the second time we hit the alittude we are go for ejection
-      if (altitudeCount == 2) {
-        stepperActivated = true; //Activate stepper and wait until it signals completion
-        if (stepperDone) {
-          digitalWrite(RELAY_PIN_1, HIGH); // Activate relay 1
-          digitalWrite(RELAY_PIN_2, HIGH); // Activate relay 2
-          Serial.println("Relays activated!");
-        }
-      }
-    }
-  }
-
-
-
-  if (stepperActivated) { //Activiates on go for eject
-    if (!stepperDone) {
-      stepper.moveTo(STEPS_PER_REV * motorRev); // Move the motor x revolutions
-      stepper.run(); //Sends signal to rotate to the stepper
-      if (stepper.distanceToGo() == 0) {//Checks to see if we are done rotating
-        stepperDone = true;
-      }
+    // if it is the second time we hit the alittude we are go for ejection
+    if (altitudeCount == 2)
+    {
+      digitalWrite(motorRelay, HIGH);
+      delay(100);
+      digitalWrite(RELAY_PIN_1, HIGH); // Activate relay 1
+      digitalWrite(RELAY_PIN_2, HIGH); // Activate relay 2
+      Serial.println("Relays activated!");
     }
   }
 }
+
